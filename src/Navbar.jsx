db32a2eb17d7611dd4f9971106a99be17457fc1b
pramit_sr <mepramitsarkarr@gmail.com>
2024-08-30 03:09:08 +0530

    AlgoVison

---
+++ src/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const listitems = [
+  { name: 'Home', link: '#home' },
+  { name: 'Features', link: 'https://prakhar0703.github.io/etch-a-sketch/' },
+  { name: 'Community', link: '#community' },
+  { name: 'About Us', link: '#about' },
+  { name: 'Contact Us', link: '#contact' }
+];
+
+const Navbar = () => {
+  return (
+    <div className='fixed bg-black flex justify-between items-center gap-14 py-3 px-13 max-w-[90%] left-1/2
+    translate-x-[-50%] top-[20px] rounded-full backdrop-blur-md bg-opacity-60 text-white shadow-lg z-10'>
+    
+        <ul className='flex gap-8 text-xl'>
+            { listitems.map((item) => (
+                <li className='relative group cursor-pointer' key={item.name}>
+                    <a href={item.link} className='hover:bg-white hover:text-black px-4 py-2
+                     rounded-full transition duration-300'>
+                      {item.name}
+                    </a>
+                </li>
+            ))}
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import Navbar from '../Navbar';
+import '../App.css'; 
+function Home() {
+  return (
+    <>
+      <Helmet>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>AlgoVision</title>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/boxicons@latest/css/boxicons.min.css"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <a href="#logo" class ="logo"><i class='bx bxs-home'></i>AlgoVision</a>
+      {/* header */}
+      {/* <header>
+        <a href="#" className="logo">
+          <i className="bx bxs-home"></i>AlgoVision
+        </a>
+        <ul className="navlist">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#features">Features</a></li>
+          <li><a href="#about">About Us</a></li>
+          <li><a href="#community">Community</a></li>
+          <li><a href="#Us">Contact Us</a></li>
+        </ul>
+        <div className="nav-icons">
+          <a href="#"><i className="bx bx-search"></i></a>
+          <div className="bx bx-menu" id="menu-icon"></div>
+        </div>
+      </header> */}
+      <Navbar/>
+      {/* Home */}
+      <section className="home" id="home">
+        <div className="home-text">
+          <h1>
+            Show, <span>Visualize &</span> <br />  Enjoy the Algorithm <br /> Visit Us
+          </h1>
+          <a href="#Home" className="button1">
+            Explore Algorithm <i className="bx bxs-right-arrow"></i>
+          </a>
+        </div>
+        <div className="home-img">
+        {/* putting img */}
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Home;
